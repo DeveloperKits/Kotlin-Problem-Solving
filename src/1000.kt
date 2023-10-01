6843e72deb37efccb00be8f5bacf9c94fcbd4f27
Akash Das <akashdas.dev@gmail.com>
2023-10-01 19:22:42 +0600

    update

--- src/1000.kt
+++ src/1000.kt
@@ -1,3 +1,19 @@
 fun main(){
-    print("Hello World!")
+    println("Hello World!")
+
+    val num = 29
+    var flag = false
+    for (i in 2..num / 2) {
+        // condition for nonprime number
+        print("i=$i, ")
+        if (num % i == 0) {
+            flag = true
+            break
+        }
+    }
+
+    if (!flag)
+        println("$num is a prime number.")
+    else
+        println("$num is not a prime number.")
 }
